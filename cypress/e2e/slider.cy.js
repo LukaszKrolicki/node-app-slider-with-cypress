69b77c26b37eacb62fb3ee51a7baa9558a1a89e7
Łukasz Królicki <lukasmatrix12345@gmail.com>
2025-03-21 12:22:52 +0100

    Test added

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,17 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Navigation Test', function () {
+  it('Should navigate to the next and previous slides', function () {
+    cy.visit('http://localhost:3000');
+
+    // Click the next button and check if the next slide is displayed
+    cy.get('.swiper-button-next').click();
+    cy.get('.swiper-slide-active').should('not.have.class', 'swiper-slide-prev');
+
+    // Click the previous button and check if the previous slide is displayed
+    cy.get('.swiper-button-prev').click();
+    cy.get('.swiper-slide-active').should('not.have.class', 'swiper-slide-next');
+  });
+});
